5b0ded152d7816f24c31faa2f3c15904f9ac363e
Guilherme Gall <gmgall@gmail.com>
2015-09-09 19:59:33 -0300

    Melhora create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
+#
+# createcontainer.sh - Cria um container docker a partir da imagem
+# gmgall/docker-rstudio que monta o diretório passado por parâmetro
+# como homedir do usuário rstudio.
+#
+# 09/09/2015 - Versão inicial
+# por Guilherme <gmgall@lncc.br>
 
 set -e
 
+IMAGE="gmgall/docker-rstudio"
+
 SHAREDIR="$1"
+
+if [ ! "$SHAREDIR" ]; then
+	echo "Informe um diretório para ser o homedir do usuário rstudio" >&2
+	exit 1
+fi
+
 if [ ! -e "$SHAREDIR" ]; then
 	echo "Diretório $SHAREDIR não existente. Criando..."
 	mkdir "$SHAREDIR"
 	chmod g+s "$SHAREDIR"
 fi
 
-CONTAINER_ID=$(docker run -d -P -v $(readlink -f "$SHAREDIR"):/home/rstudio gmgall/test)
-echo Porta $(docker inspect --format='{{ (index (index .NetworkSettings.Ports "8787/tcp") 0).HostPort }}' $CONTAINER_ID)
+CONTAINER_ID=$(docker run -d -P -v $(readlink -f "$SHAREDIR"):/home/rstudio "$IMAGE")
+PORT=$(docker inspect --format='{{ (index (index .NetworkSettings.Ports "8787/tcp") 0).HostPort }}' $CONTAINER_ID)
+
+echo "Acesse o container em $(hostname -f):$PORT"
+echo "O homedir do usuário rstudio está montado em $SHAREDIR"
